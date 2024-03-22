6f2e2ce73ff83beb68d525587884bb6e2ed04741
ibrahimmuhaned <ibrahimmuhaned999@gmail.com>
2024-03-22 03:24:17 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code in this file!
+
+const currentUser = "John";
+
+function getFirstInitial(name) {
+    return name.charAt(0).toUpperCase();
+}
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+const shortGreeting = `Welcome, ${getFirstInitial(currentUser)}!`;
+
+console.log(welcomeMessage);
+console.log(excitedWelcomeMessage);
+console.log(shortGreeting);
